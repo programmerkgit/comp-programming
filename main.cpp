5b99c7e7ba123b3b6dbced50e7833913a6cd3e2f
keishi kubo <37021071+programmerkgit@users.noreply.github.com>
2021-11-29 01:39:59 +0900

    fix import

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-
-#include <iostream>
-#include "aizdu-online/1/DMaximumProfit.h"
-#include "aoj_lessonitp/1/DWatch.h"
-#include "aoj_lessonitp/2/ASmallLargeequal.h"
-#include "aoj_lessonitp/2/BRange.h"
-#include "aoj_lessonitp/2/CSortThreeNumbers.h"
-#include "aoj_lessonitp/3/BPrintTestCases.h"
-#include "aoj_lessonitp/3/CSwappingTwoNumbers.h"
 #include "aoj_lessonitp/3/DHowManyDivisors.h"
-
-
-using namespace std;
 
 int main() {
     DHowManyDivisors::main();
